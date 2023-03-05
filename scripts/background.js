6a5168488a5ee61d07df71878e051b35397b8723
Ioscipescu <blountwy@gmail.com>
2023-03-05 10:59:59 -0700

    Final version-no AI + bert training

--- scripts/background.js
+++ scripts/background.js
@@ -8,19 +8,41 @@
 // const webstore = "https://developer.chrome.com/docs/webstore";
 
 chrome.webNavigation.onCompleted.addListener((tab) => {
-    // if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    if (tab.url.startsWith("https://www.nytimes.com/")) {
-        // chrome.action.setBadgeText({
-        //     text: "ON",
-        // });
-        // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-        chrome.notifications.create("Left Leaning Notification", {
-            type: "basic",
-            iconUrl: chrome.runtime.getURL("images/logo-small.png"),
-            title: "Left Leaning Source Detected",
-            message:
-                "The source that you are using has been detected to contain liberal leaning terminology, remember to look at both sides of any issue before making decisions.",
-            priority: 2,
-        });
-    }
+  // if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+  if (
+    tab.url.startsWith("https://www.nytimes.com/") ||
+    tab.url.startsWith("https://www.cnn.com/") ||
+    tab.url.startsWith("https://www.palmerreport.com/")
+  ) {
+    // chrome.action.setBadgeText({
+    //     text: "ON",
+    // });
+    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+    chrome.notifications.create("Left Leaning Notification", {
+      type: "basic",
+      iconUrl: chrome.runtime.getURL("images/logo-small.png"),
+      title: "Left Leaning Source Detected",
+      message:
+        "The source that you are using has been detected to contain liberal leaning terminology, remember to look at both sides of any issue before making decisions.",
+      priority: 2,
+    });
+  }
+  if (
+    tab.url.startsWith("https://www.foxnews.com/") ||
+    tab.url.startsWith("https://www.dailywire.com/") ||
+    tab.url.startsWith("https://www.dailysignal.com/")
+  ) {
+    // chrome.action.setBadgeText({
+    //     text: "ON",
+    // });
+    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+    chrome.notifications.create("Right Leaning Notification", {
+      type: "basic",
+      iconUrl: chrome.runtime.getURL("images/logo-small.png"),
+      title: "Right Leaning Source Detected",
+      message:
+        "The source that you are using has been detected to contain republican leaning terminology, remember to look at both sides of any issue before making decisions.",
+      priority: 2,
+    });
+  }
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,113 +1,108 @@
 function getText() {
-    return document.body.innerText;
+  return document.body.innerText;
 }
 
 function getHTML() {
-    return document.body.outerHTML;
+  return document.body.outerHTML;
 }
 
 function getArticleContents() {
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(getHTML(), "text/html");
-    let articleContent = parseHtml(htmlDoc, "meteredContent"); //NYTimes (left)
-    if (articleContent != null) {
-        return articleContent;
-    }
-    articleContent = parseHtml(htmlDoc, "article__content"); //CNN (left)
-    if (articleContent != null) {
-        return articleContent;
-    }
-    articleContent = parseHtml(htmlDoc, "article-body"); //Fox News (right)
-    if (articleContent != null) {
-        return articleContent;
-    }
-    articleContent = parseHtml(htmlDoc, "post_content__w3pdf"); //Buzzfeed News (left)
-    if (articleContent != null) {
-        return articleContent;
-    }
-    articleContent = parseHtml(htmlDoc, "e1nzmkc90 css-15jw47d"); //Daily Wire (right)
-    if (articleContent != null) {
-        return articleContent;
-    }
-    articleContent = parseHtml(htmlDoc, "mh-wrapper clearfix"); //News Punch (extreme right)
-    if (articleContent != null) {
-        return articleContent;
-    }
-    articleContent = parseHtml(htmlDoc, "tds-content"); //The Daily Signal (right)
-    if (articleContent != null) {
-        return articleContent;
-    }
-    articleContent = parseHtml(htmlDoc, "fl-post-content clearfix"); //Palmer Report (extreme left)
-    if (articleContent != null) {
-        return articleContent;
-    }
-    articleContent = parseHtml(htmlDoc, "c-entry-content"); //Vox (left)
-    if (articleContent != null) {
-        return articleContent;
-    }
+  const parser = new DOMParser();
+  const htmlDoc = parser.parseFromString(getHTML(), "text/html");
+  let articleContent = parseHtml(htmlDoc, "meteredContent"); //NYTimes (left)
+  if (articleContent != null) {
+    return articleContent;
+  }
+  articleContent = parseHtml(htmlDoc, "article__content"); //CNN (left)
+  if (articleContent != null) {
+    return articleContent;
+  }
+  articleContent = parseHtml(htmlDoc, "article-body"); //Fox News (right)
+  if (articleContent != null) {
+    return articleContent;
+  }
+  articleContent = parseHtml(htmlDoc, "post_content__w3pdf"); //Buzzfeed News (left)
+  if (articleContent != null) {
+    return articleContent;
+  }
+  articleContent = parseHtml(htmlDoc, "e1nzmkc90 css-15jw47d"); //Daily Wire (right)
+  if (articleContent != null) {
+    return articleContent;
+  }
+  articleContent = parseHtml(htmlDoc, "mh-wrapper clearfix"); //News Punch (extreme right)
+  if (articleContent != null) {
+    return articleContent;
+  }
+  articleContent = parseHtml(htmlDoc, "tds-content"); //The Daily Signal (right)
+  if (articleContent != null) {
+    return articleContent;
+  }
+  articleContent = parseHtml(htmlDoc, "fl-post-content clearfix"); //Palmer Report (extreme left)
+  if (articleContent != null) {
+    return articleContent;
+  }
+  articleContent = parseHtml(htmlDoc, "c-entry-content"); //Vox (left)
+  if (articleContent != null) {
+    return articleContent;
+  }
 
-    articleContent = parseHtml(htmlDoc, "dcr-h26idz"); //Guardian (left)
-    if (articleContent != null) {
-        return articleContent;
-    }
-    
-
-
+  articleContent = parseHtml(htmlDoc, "dcr-h26idz"); //Guardian (left)
+  if (articleContent != null) {
+    return articleContent;
+  }
 }
 
 function parseHtml(htmlDoc, text) {
-    for (const element of htmlDoc.getElementsByClassName(text)) {
-        if (element != null) {
-            return element.innerText;
-        }
+  for (const element of htmlDoc.getElementsByClassName(text)) {
+    if (element != null) {
+      return element.innerText;
     }
+  }
 }
 
 function formatText(text) {
-    if (text != undefined)
-        return text
-            .replace(/\s+/g, " ")
-            .replace(/[^a-zA-Z0-9"';:!?()-_,. ]/g, "");
+  if (text != undefined)
+    return text.replace(/\s+/g, " ").replace(/[^a-zA-Z0-9"';:!?()-_,. ]/g, "");
 }
 
 function sentenceArray(text) {
-    if (text != undefined) return text.split(". ");
+  if (text != undefined) return text.split(". ");
 }
 
 function predict(text) {
-    if (text != undefined) {
-        return (async () => {
-            const onnx = await import("onnx");
+  if (text != undefined) {
+    return (async () => {
+      const onnx = await import("onnxjs");
 
-            const modelPath = "model.onnx";
-            const model = await onnx.load(modelPath);
+      const modelPath = "model.onnx";
+      const model = await onnx.load(modelPath);
 
-            const inputIds = tokenizer.encode(text);
+      const inputIds = tokenizer.encode(text);
 
-            const inputTensor = new onnx.Tensor(
-                new Int32Array(inputIds),
-                "int32",
-                [1, inputIds.length]
-            );
+      const inputTensor = new onnx.Tensor(new Int32Array(inputIds), "int32", [
+        1,
+        inputIds.length,
+      ]);
 
-            const output = await model.run({ input: inputTensor });
-            return output.values().next().value.data;
-        })();
-    }
+      const output = await model.run({ input: inputTensor });
+      console.log(output);
+      return output.values().next().value.data;
+    })();
+  }
 }
 
 function generateSentiment(text) {
-    if (text != undefined) {
-        let array = [];
-        for (const sentence of sentenceArray(text)) {
-            array.push(predict(sentence));
-        }
-        let total = 0;
-        for (let i of array) {
-            total += i;
-        }
-        return total / array.length;
+  if (text != undefined) {
+    let array = [];
+    for (const sentence of sentenceArray(text)) {
+      array.push(predict(sentence));
     }
+    let total = 0;
+    for (let i of array) {
+      total += i;
+    }
+    return total / array.length;
+  }
 }
 
 // let logo = document.getElementById("logo");
