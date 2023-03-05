f3df73e17d1aa11cf92be53874c7a9b73aabf883
Ioscipescu <blountwy@gmail.com>
2023-03-04 22:39:06 -0700

    forgot to add background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,24 +1,25 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-        text: "OFF",
-    });
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.action.setBadgeText({
+//         text: "OFF",
+//     });
+// });
 
-const extensions = "https://developer.chrome.com/docs/extensions";
-const webstore = "https://developer.chrome.com/docs/webstore";
+// const extensions = "https://developer.chrome.com/docs/extensions";
+// const webstore = "https://developer.chrome.com/docs/webstore";
 
 chrome.webNavigation.onCompleted.addListener((tab) => {
-    if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-        chrome.action.setBadgeText({
-            text: "ON",
-        });
+    // if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+    if (tab.url.startsWith("https://www.nytimes.com/")) {
+        // chrome.action.setBadgeText({
+        //     text: "ON",
+        // });
         // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
         chrome.notifications.create("Left Leaning Notification", {
             type: "basic",
             iconUrl: chrome.runtime.getURL("images/logo-small.png"),
             title: "Left Leaning Source Detected",
             message:
-                "The source that you are using has been detected to be heavily left leaning with a confidence of _____%",
+                "The source that you are using has been detected to contain liberal leaning terminology, remember to look at both sides of any issue before making decisions.",
             priority: 2,
         });
     }
